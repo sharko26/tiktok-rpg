a9962216a445b1e6f7c77c75e1810f52b32608d6
sharko26 <84505595+sharko26@users.noreply.github.com>
2025-04-13 11:26:43 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +9,16 @@ CORS(app)
 def index():
     return "Serveur TikTok RPG en ligne !"
 
+# Route pour l'overlay
+@app.route("/frontend/<path:filename>")
+def overlay(filename):
+    return send_from_directory("frontend", filename)
+
+# Route pour le panel admin
+@app.route("/admin_panel/<path:filename>")
+def admin(filename):
+    return send_from_directory("admin_panel", filename)
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
