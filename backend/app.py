24ebc1c115e56538a5122d7e20c4a8d37d79df8f
sharko26 <84505595+sharko26@users.noreply.github.com>
2025-04-13 11:13:53 +0200

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+    return "Serveur TikTok RPG en ligne !"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
